2ac4cbc6aed4004a283c4799c797e3f57a0e9e00
zptilden325 <71046087+zptilden325@users.noreply.github.com>
2022-03-22 14:48:05 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,39 @@
+/**
+ * 8 Concepts Learned (Pick the 8 you used, delete the others):
+ * 
+ * -Sprites
+ * 
+ * -Sprite Position (coordinates: x and y)
+ * 
+ * -Controller (dx) and move mySprite with buttons
+ * 
+ * -overlap Events / sprite kind
+ * 
+ * -"Spawning" sprites: create and on created
+ * 
+ * -random
+ * 
+ * -Displaying numbers (1, 2, 3) as a string ("123")
+ * 
+ * -score and life
+ * 
+ * -countdown
+ * 
+ * -stay in screen and ghost
+ * 
+ * -set image
+ * 
+ * -sprite say
+ * 
+ * -splash
+ * 
+ * Key Features (List the Key Features You Will Need For Your Game, replace examples)
+ * 
+ * - ex. Playable Sprite
+ * 
+ * - ex. player can move up/down left/right
+ * 
+ * - ex. player can collide with enemy
+ * 
+ * - ex. enemy decreases player life
+ */
